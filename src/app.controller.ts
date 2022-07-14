aebbc82ffd648134e15c56928348d03fafd8bb25
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2022-07-14 20:15:02 +0300

    Local authentication strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
