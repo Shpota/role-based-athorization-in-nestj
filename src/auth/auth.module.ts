229d75da4b17ca607bc79bbb16173e7297e3422b
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2022-07-14 16:10:20 +0300

    Auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
